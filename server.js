6395acd3a8b5f180a4261ae4b444b4740cb1300c
riyapahuja20 <riyapahuja20@yahoo.com>
2018-06-12 17:39:27 +0530

    Change server

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('/dist/ang6-heroku-frontend'));
-console.log(__dirname);
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join('/dist/ang6-heroku-frontend/index.html'));
-console.log(__dirname);
-});
+// Run the app by serving the static files in the dist directory
+app.use(express.static('/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
