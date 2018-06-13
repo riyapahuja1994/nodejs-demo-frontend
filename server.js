71058892e8808bb5484be7f2f04d042895d58ad3
riyapahuja20 <riyapahuja20@yahoo.com>
2018-06-13 08:59:41 +0530

    Reverted angular.json and server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static('/dist/ang6-heroku-frontend'));
+console.log(__dirname);
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join('/dist/ang6-heroku-frontend/index.html'));
+console.log("Path"+path);
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
