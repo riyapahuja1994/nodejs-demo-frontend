5551e4d8b78431a539f95122b51f6b2de7b6d0f4
riyapahuja20 <riyapahuja20@yahoo.com>
2018-06-13 12:27:08 +0530

    Changed server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ang6-heroku-frontend'));
+app.use(express.static(__dirname + '/dist'));
 console.log(__dirname);
 
 // app.get('/*all', function(req,res) {
